c065bdcbf53317edfc63f5790d00e5be487548a2
Acer611 <Y_gaofei@163.com>
2018-07-02 23:12:15 +0800

    注释bean 消除启动时报定时任务bean找不到的警告（debug级别）

--- src/main/java/com/style/slack/config/SessionConfig.java
+++ src/main/java/com/style/slack/config/SessionConfig.java
@@ -3,10 +3,13 @@ package com.style.slack.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 
 /***
+ * create by Gaofei 2018/07/02
  * Session 共享
  *  分布式系统中session共享有很多不错的解决方案，
  *  其中托管到缓存中是比较常见的方案之一，
@@ -20,5 +23,13 @@ public class SessionConfig {
     @Bean
     public JedisConnectionFactory connectionFactory() {
         return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public TaskScheduler scheduledExecutorService() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(8);
+        scheduler.setThreadNamePrefix("scheduled-thread-");
+        return scheduler;
     }
 }
