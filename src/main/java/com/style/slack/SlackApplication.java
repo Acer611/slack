79d886c86f0854c25dad8b5c660d705a13f03361
Gaofei <y_gaofei@163.com>
2018-08-15 17:43:14 +0800

    添加支持外部独立tomcat部署所需要的依赖和配置

--- src/main/java/com/style/slack/SlackApplication.java
+++ src/main/java/com/style/slack/SlackApplication.java
@@ -3,6 +3,8 @@ package com.style.slack;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -11,10 +13,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //扫描dao
 @MapperScan("com.style.slack.dao.*")
 @EnableScheduling
-public class SlackApplication {
+public class SlackApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         //等价于 new SpringApplication(SpringBootApplication.class).run(args);
         SpringApplication.run(SlackApplication.class,args);
     }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SlackApplication.class);
+    }
+
 }
